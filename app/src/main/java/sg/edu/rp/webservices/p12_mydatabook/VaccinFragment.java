0777325b50f08b8d3f99be77518adc3e34d98eef
16022877 <16022877@myrp.edu.sg>
2018-08-10 01:24:12 +0800

    dcdscjfj

---
+++ app/src/main/java/sg/edu/rp/webservices/p12_mydatabook/VaccinFragment.java
@@ -0,0 +1,29 @@
+package sg.edu.rp.webservices.p12_mydatabook;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class VaccinFragment extends Fragment {
+
+
+    public VaccinFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vaccin, container, false);
+    }
+
+}
